37b5cccd0fadd9043fba1d0a453a805d4eecdd19
Allen Deckert <adeckert1010@gmail.com>
2020-03-05 11:19:52 -0500

    first commit

---
+++ MaintenanceManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MaintenanceManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
