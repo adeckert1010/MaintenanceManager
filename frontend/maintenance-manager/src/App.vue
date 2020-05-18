a97c2673a58cbe8fb88d58838a5b749cefac4b55
adeckert1010 <adeckert1010@gmail.com>
2020-05-18 14:23:49 -0400

    Got api calls working. Shows images

--- frontend/maintenance-manager/src/App.vue
+++ frontend/maintenance-manager/src/App.vue
@@ -1,13 +1,8 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      dark
-      class="secondary"
-    >
+    <v-navigation-drawer v-model="drawer" app dark mini-variant expand-on-hover class="secondary">
       <v-list dense>
-        <v-list-item link>
+        <v-list-item link :to="'/'">
           <v-list-item-action>
             <v-icon>mdi-home</v-icon>
           </v-list-item-action>
@@ -15,48 +10,41 @@
             <v-list-item-title>Home</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item link>
+        <v-list-item link :to="'addcar'">
           <v-list-item-action>
-            <v-icon>mdi-mail</v-icon>
+            <v-icon>mdi-car</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
+            <v-list-item-title>Add A Car</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      class="secondary"
-      dark
-    >
+    <v-app-bar app class="secondary" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Maintenance Manager</v-toolbar-title>
+      <v-toolbar-title v-cloak>Maintenance Manager</v-toolbar-title>
     </v-app-bar>
-    
+
     <v-content class="primary">
-      <router-view/>
+      <router-view />
     </v-content>
-    <v-footer
-      color="secondary"
-      app
-    >
+    <v-footer color="secondary" app>
       <span class="white--text">&copy; {{new Date().getFullYear()}} Allen Deckert</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-  export default {
-    props: {
-      source: String,
-    },
-    data() {
-      return {
-         drawer: null,
-         initialDark: this.$vuetify.theme.dark
-      }
-    },
+export default {
+  props: {
+    source: String
+  },
+  data() {
+    return {
+      drawer: null,
+      initialDark: this.$vuetify.theme.dark
+    };
   }
+};
 </script>
--- frontend/maintenance-manager/src/views/AddCar.vue
+++ frontend/maintenance-manager/src/views/AddCar.vue
@@ -1,17 +1,48 @@
 <template>
-  <container class="fill-height" fluid>
+  <v-container class="fill-height" fluid>
     <v-row align="center" justify="center">
       <v-col cols="12" sm="8" md="6">
         <v-card class="elevation-12">
           <v-toolbar color="secondary">
             <v-toolbar-title>Choose your car</v-toolbar-title>
           </v-toolbar>
+          <v-img id="carImage" v-show="name" :src="imgSrc" transition :lazy-src="require('@/assets/Transpo_G70_TA-518126.jpg')">
+          <template v-slot:placeholder>
+        <v-row
+          class="fill-height ma-0"
+          align="center"
+          justify="center"
+        >
+          <v-progress-circular v-if="name" indeterminate color="grey lighten-5"></v-progress-circular>
+        </v-row>
+          </template>
+</v-img>
           <v-card-text>
+            <p>{{year}} {{make}} {{name}}</p>
             <v-form>
               <v-select
-              v-show="makes"
-              :items="makes"
-              :item-text="makes.Make_Name"
+                :items="years"
+                v-model="year"
+                label="Select A Year"
+                @change="getMakesByYear(); clearMake(); clearName();"
+              ></v-select>
+              <v-select
+                v-if="year"
+                v-model="make"
+                label="Select A Make"
+                :items="makes"
+                :loading="!makes"
+                :disabled="!makes"
+                @change="getModelsByYearAndMake(); clearName();"
+              ></v-select>
+              <v-select
+                v-if="make"
+                v-model="name"
+                label="Select A Model"
+                :items="models"
+                :loading="!models"
+                :disabled="!models"
+                @change="getPicture()"
               ></v-select>
             </v-form>
           </v-card-text>
@@ -22,27 +53,138 @@
         </v-card>
       </v-col>
     </v-row>
-  </container>
+  </v-container>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      makes: []
+      models: null,
+      makes: null,
+      years: [
+        "2020",
+        "2019",
+        "2018",
+        "2017",
+        "2016",
+        "2015",
+        "2014",
+        "2013",
+        "2012",
+        "2011",
+        "2010",
+        "2009",
+        "2008",
+        "2007",
+        "2006",
+        "2005",
+        "2004",
+        "2003",
+        "2002",
+        "2001",
+        "2000",
+        "1999",
+        "1998",
+        "1997",
+        "1996"
+      ],
+      year: null,
+      make: "",
+      name: "",
+      imgSrc: ""
     };
   },
-  created() {
-    //retrieve list of makes from API
-    fetch("https://vpic.nhtsa.dot.gov/api/vehicles/getallmanufacturers?ManufacturerType=complete&format=json")
-      .then(response => {
-        return response.json();
+  created() {},
+  methods: {
+    getMakesByYear() {
+      //retrieve list of makes from API
+      const headers = new Headers();
+      headers.append(
+        "Authorization",
+        "Basic NzhhZTlkY2YtYzY5YS00NmMzLTg1NjAtY2YzMDBiNDI0ZWQy"
+      );
+      headers.append("partner-token", "ae89a034867a458cb665133a5ad4e8a7");
+      fetch(`https://api.carmd.com/v3.0/make?year=${this.year}`, {
+        method: "GET",
+        headers: headers
       })
-      .then(x => {
-        this.makes = x.results;
-      })
-      .catch(err => console.error(err));
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then(x => {
+          this.makes = x.data;
+        })
+        .catch(err => console.error(err));
+    },
+
+    getModelsByYearAndMake() {
+      //retrieve list of models from API
+      const headers = new Headers();
+      headers.append(
+        "Authorization",
+        "Basic NzhhZTlkY2YtYzY5YS00NmMzLTg1NjAtY2YzMDBiNDI0ZWQy"
+      );
+      headers.append("partner-token", "ae89a034867a458cb665133a5ad4e8a7");
+      fetch(
+        `https://api.carmd.com/v3.0//model?year=${this.year}&make=${this.make}`,
+        {
+          method: "GET",
+          headers: headers
+        }
+      )
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then(x => {
+          this.models = x.data;
+        })
+        .catch(err => console.error(err));
+    },
+    getPicture() {
+      const oReq = new XMLHttpRequest();
+      let self = this;
+      self.imgSrc="@/assets/Transpo_G70_TA-518126.jpg"
+      oReq.onreadystatechange = function() {
+        if (oReq.readyState == 4 && oReq.status == 200) {
+          self.setImgSrc(oReq);
+        }
+      };
+      oReq.open(
+        "GET",
+        `http://www.carimagery.com/api.asmx/GetImageUrl?searchTerm=${this.year}+${this.make}+${this.name}`,
+        true
+      );
+      oReq.send();
+    },
+    setImgSrc: function(xml) {
+      let self = this;
+      const xmlDoc = xml.responseXML;
+      const x = xmlDoc.getElementsByTagName("string");
+      console.log(x[0].firstChild.data);
+      self.imgSrc = x[0].firstChild.data;
+    },
+    clearName() {
+      this.name = "";
+    },
+    clearMake() {
+      this.make = "";
+    }
   }
+  // computed: {
+  //   loadingState() {
+  //     if(this.makes!==0){
+  //       return false
+  //     }
+  //     else {
+  //       return true
+  //     }
+  //   }
+  // }
 };
 </script>
 
